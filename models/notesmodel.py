58e38c07fb1b4a21aba3ba5e027e761b7cfa6e22
shuler7 <vitalisobolevggg@gmail.com>
2025-10-13 21:06:49 +0300

    Add ForeignKey: notes.uid -> user.uid

--- models/notesmodel.py
+++ models/notesmodel.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, ARRAY
+from sqlalchemy import Integer, String, ARRAY, ForeignKey
 from database import Base
 
 
 class NotesModel(Base):
     __tablename__ = "notes"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    uid: Mapped[int] = mapped_column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    uid: Mapped[int] = mapped_column(Integer, ForeignKey("user.uid", name='fk_notes_user_uid'), nullable=False)
     title: Mapped[str] = mapped_column(String, nullable=False)
     text: Mapped[str] = mapped_column(String, nullable=False)
     status: Mapped[str] = mapped_column(String, nullable=False)
--- models/usermodel.py
+++ models/usermodel.py
@@ -6,6 +6,6 @@ from database import Base
 class UserModel(Base):
     __tablename__ = "user"
 
-    uid: Mapped[int] = mapped_column(Integer, primary_key=True)
+    uid: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
